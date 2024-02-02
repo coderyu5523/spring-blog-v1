806c2f60264070f47034eb022972055f7c7d6cf1
coderyu5523 <coderyu5523@naver.com>
2024-02-02 09:16:09 +0900

    블러로그 쌤코드로 수정

--- src/main/java/shop/mtcoding/blog/board/BoardController.java
+++ src/main/java/shop/mtcoding/blog/board/BoardController.java
@@ -19,41 +19,21 @@ public class BoardController {
 
     //@RequestParam(defaultValue = "0") 이거는 값을 안넣으면 페이지=0 으로 설정
     @GetMapping({ "/", "/board" })
-    public String index(HttpServletRequest request , @RequestParam(defaultValue = "0") int page) {
-        System.out.println("페이지"+page);
-
-
-        // 스프링의 기본 전략은 포워드
+    public String index(HttpServletRequest request, @RequestParam(defaultValue = "0") int page) {
         List<Board> boardList = boardRepository.findAll(page);
-        request.setAttribute("boardList",boardList);  // 가방에 담기
-        // 가방에 담기
+        request.setAttribute("boardList", boardList);
 
-        int curruentPage =page ;
-        int nextPage = curruentPage +1 ;
-        int prevPage = curruentPage -1 ;
+        int currentPage = page;
+        int nextPage = currentPage+1;
+        int prevPage = currentPage-1;
+        request.setAttribute("nextPage", nextPage);
+        request.setAttribute("prevPage", prevPage);
 
-        request.setAttribute("nextPage",nextPage);  // 가방에 담기
-        request.setAttribute("prevPage",prevPage);  // 머스태치에 담기만 하면 됨
+        boolean first = shop.mtcoding.blog._core.PagingUtil.isFirst(currentPage);
+        boolean last = shop.mtcoding.blog._core.PagingUtil.isLast(currentPage, 4);
 
-
-        boolean first = (curruentPage==0 ?true :false);
-        request.setAttribute("first",first);
-
-        int totalCount = 4; // 나중에는 db에서 전체 데이터를 조회해야 됨. 그래야 라스트페이지를 알 수 있음
-
-        int totalPage = totalCount/3 ;
-        if(totalCount%3==0){
-            int lastPage = totalPage -1 ;
-            boolean last = (curruentPage==lastPage? true:false);
-
-            request.setAttribute("last",last);
-
-        } else if(totalCount%3!=0){
-            int lastPage = totalPage ;
-            boolean last = (curruentPage==lastPage? true:false);
-            request.setAttribute("last",last);
-        }
-
+        request.setAttribute("first", first);
+        request.setAttribute("last", last);
 
         return "index";
     }
