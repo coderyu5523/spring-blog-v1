fb0e0c5431e457a95482c3f3cd4dac6275a0cf9c
coderyu5523 <coderyu5523@naver.com>
2024-02-02 14:36:18 +0900

    게시글 권한

--- src/main/java/shop/mtcoding/blog/board/BoardController.java
+++ src/main/java/shop/mtcoding/blog/board/BoardController.java
@@ -7,13 +7,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
+import shop.mtcoding.blog.user.User;
 
 import java.util.List;
 
 @RequiredArgsConstructor
 @Controller
 public class BoardController {
-
+    //Ioc컨테이너에 세션에 접근할 수 있는 변수가 들어가 있음. DI 하면 됨
     private final HttpSession session;
     private final BoardRepository boardRepository ;
 
@@ -48,6 +49,23 @@ public class BoardController {
     public String detail(@PathVariable int id, HttpServletRequest request) { // {} 를 알아서 파싱해줌
         BoardResponse.DetailDTO responseDTO = boardRepository.findById(id);
         request.setAttribute("board",responseDTO);
+
+        //1. 해당 페이지의 주인 여부
+        boolean owner = false ;
+
+        //2 . 작성자 userId 확읺기
+        int boardUserId = responseDTO.getUserId();
+
+        //3. 로그인 여부 체크 / 로그인이 안되면 무조건 false, 권한 체크
+        User sessionUser = (User) session.getAttribute("sessionUser"); // setAttribute 에서 키를 sessionUser 로 정함
+        if(sessionUser !=null){  // 로그인을 했음
+            if(boardUserId == sessionUser.getId()){
+                owner =true ;
+            }
+
+
+        }
+        request.setAttribute("owner",owner);
         return "board/detail";
     }
 
